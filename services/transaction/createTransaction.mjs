59ca2224e930e727af0420bfe94aa6e3d1a949f0
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-23 13:00:24 +0630

    update create transaction with account balance update

--- services/transaction/createTransaction.mjs
+++ services/transaction/createTransaction.mjs
@@ -5,13 +5,56 @@ import { responseConstructor } from "../../utils/common.mjs"
 
 const prisma = new PrismaClient
 
+async function addTransactionAndUpdateBalance(transactionData) {
+    const { amount, accountId, spentType, ...rest } = transactionData;
+
+    // Start a Prisma transaction
+    const transaction = await prisma.$transaction(async (prisma) => {
+      // Add the new transaction
+      const newTransaction = await prisma.transaction.create({
+        data: {
+          amount,
+          accountId,
+          spentType,
+          ...rest
+        },
+      });
+  
+      // Get the current balance of the account
+      const account = await prisma.account.findFirst({
+        where: { id: accountId },
+      });
+  
+      // Update the balance in the account
+      const updatedAccount = await prisma.account.update({
+        where: { id: accountId },
+        data: {
+          balance: spentType === "in" ? account.balance + amount : account.balance - amount ,
+        },
+      });
+  
+      return { newTransaction, updatedAccount };
+    });
+  
+    return transaction;
+  }
+
 const createTransaction = async (event) => {
-    const result = await prisma.transaction.create({data: {...event.body, updatedAt: new Date()}})
+    let response = addTransactionAndUpdateBalance({...event.body, updatedAt: new Date()})
+        .then((result) => {
+            console.log('Transaction added and account balance updated:', result);
+            return responseConstructor(200, {message: 'success'})
 
-    const response = responseConstructor(200, result)
+        })
+        .catch((error) => {
+            console.error('Error adding transaction:', error);
+            return responseConstructor(500, {message: 'fail'})
 
+        })
+        .finally(async () => {
+            await prisma.$disconnect();
+        });
     return response;
-    
 }
 
 const handler = middy(createTransaction).use(httpJsonBodyParser())
