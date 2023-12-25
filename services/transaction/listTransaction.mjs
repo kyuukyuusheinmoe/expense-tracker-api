42af9416560cc3b24533c39c50bf143680d22bb3
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-25 15:10:58 +0630

    add pagination and order desc

--- services/transaction/listTransaction.mjs
+++ services/transaction/listTransaction.mjs
@@ -4,11 +4,21 @@ import { responseConstructor } from "../../utils/common.mjs"
 
 const prisma = new PrismaClient()
 
-const listTransaction = async () => {
-    const list = await prisma.transaction.findMany({include: {
+const listTransaction = async (event) => {
+  const {pageSize, page} = event.queryStringParameters;
+    const list = await prisma.transaction.findMany({
+      skip: page * pageSize,
+      take: +pageSize,
+      include: {
         account: {select: {name:true, accountType :true }},
         category: {select: {label:true }},
-      },})
+      },
+      orderBy: [
+        {
+          createdAt: 'desc',
+        },
+      ],
+    })
 
     const response = responseConstructor(200, list);
     return response
