c384f46cbf5903270aed327e8775db8eaf35a6cc
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-27 22:16:38 +0630

    add authHandler

--- services/account/listAccount.mjs
+++ services/account/listAccount.mjs
@@ -1,15 +1,21 @@
 import { PrismaClient } from "@prisma/client"
 import { responseConstructor } from "../../utils/common.mjs"
+import middy from "middy"
+import { authHandler } from "../../middleware/authMiddleware.mjs"
+import httpJsonBodyParser from '@middy/http-json-body-parser'
 
 const prisma = new PrismaClient()
-const listAccounts = async () => {
-    const result = await prisma.account.findMany()
+const listAccounts = async (event) => {
+    const {auth} = event;
+    const result = await prisma.account.findMany({where: {
+        userId: auth.id
+    }})
 
     const response = responseConstructor(200, result)
 
     return response
 }
 
-const handler  = listAccounts
+const handler  = middy(listAccounts).use(httpJsonBodyParser()).use(authHandler())
 
 export {handler}
