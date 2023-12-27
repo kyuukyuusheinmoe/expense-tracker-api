e9d284d5e744ca91ea8eb846080acf9bafb8532e
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-27 22:15:07 +0630

    add authHandler createAccount

--- services/account/createAccount.mjs
+++ services/account/createAccount.mjs
@@ -2,17 +2,19 @@ import { PrismaClient } from "@prisma/client"
 import middy from "middy"
 import httpJsonBodyParser from '@middy/http-json-body-parser'
 import { responseConstructor } from "../../utils/common.mjs"
+import { authHandler } from "../../middleware/authMiddleware.mjs"
 
 const prisma = new PrismaClient
 
 const createAccount = async (event) => {
+    const {auth} = event;
     const body = event.body
-    const result = await prisma.account.create({data: {...body, updatedAt: new Date()}})
+    const result = await prisma.account.create({data: {...body, userId: auth.id, updatedAt: new Date()}})
     const response = responseConstructor(200, result)
 
     return response
 }
 
-const handler = middy(createAccount).use(httpJsonBodyParser())
+const handler = middy(createAccount).use(httpJsonBodyParser()).use(authHandler())
 
 export {handler}
