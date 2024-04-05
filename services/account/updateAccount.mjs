f25f6701e392a08cd47121555541a71f00a09649
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-04-05 13:00:52 +0630

    add update Account

--- services/account/updateAccount.mjs
+++ services/account/updateAccount.mjs
@@ -2,18 +2,24 @@ import { PrismaClient } from "@prisma/client"
 import middy from "middy"
 import httpJsonBodyParser from '@middy/http-json-body-parser'
 import { responseConstructor } from "../../utils/common.mjs"
+import { authHandler } from "../../middleware/authMiddleware.mjs"
 
-//TODO: make it work
+
 const prisma = new PrismaClient
 
 const updateAccount = async (event) => {
+    const {auth, pathParameters} = event;
     const body = event.body
-    const result = await prisma.account.update({data: {...body, updatedAt: new Date()}})
+    const result = await prisma.account.update(
+        {   
+            where: {id: parseInt(pathParameters.id), userId: auth.id},
+            data: {...body, updatedAt: new Date()}
+    })
     const response = responseConstructor(200, result)
 
     return response
 }
 
-const handler = middy(updateAccount).use(httpJsonBodyParser())
+const handler = middy(updateAccount).use(httpJsonBodyParser()).use(authHandler())
 
 export {handler}
